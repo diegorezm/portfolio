4dd3b01095b0d45536aeb390eae40f27a6b39107
diegorezm <dmourarezende@gmail.com>
2024-11-07 17:51:02 -0300

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,15 @@ import icon from 'astro-icon';
 
 import alpinejs from '@astrojs/alpinejs';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   integrations: [tailwind({
     applyBaseStyles: false
-  }), icon(), alpinejs()]
+  }), icon(), alpinejs()],
+
+  adapter: netlify()
 });

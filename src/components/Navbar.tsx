53dbdf30bc724cfa3a4b68947ffc77eca16d02e6
diegorezm <dmourarezende@gmail.com>
2025-04-01 12:06:04 -0300

    fixing links order

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,9 @@ import { useState, useEffect } from "preact/hooks";
 
 const navLinks = [
   { name: "Home", href: "#hero" },
+  { name: "About", href: "#about" },
   { name: "Projects", href: "#projects" },
   { name: "Contact", href: "#contact" },
-  { name: "About", href: "#about" },
 ];
 
 function MobileNavbar() {

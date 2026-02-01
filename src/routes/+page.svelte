a579f8b57faba53129f641fee8b63dd33f0b1b1a
diegorezm <dmourarezende@gmail.com>
2026-02-01 17:54:20 -0300

    cat bem_vindo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,16 +6,22 @@
   <h1
     class="text-4xl font-bold before:content-['$'] before:text-dark-yellow before:mr-2"
   >
-    <span class="text-dark-yellow">echo</span> "Bem vindo!"
+    <span class="text-dark-yellow">cat</span> bem_vindo.txt
   </h1>
-  <p class="italic text-md">
-    Meu nome é Diego, eu sou um desenvolvedor fullstack<span
-      class="text-dark-yellow blink">_</span
-    >
-  </p>
+
+  <div
+    class="border-l-2 border-dark-yellow pl-3 font-mono text-md text-grey leading-relaxed space-y-2"
+  >
+    <h2 class="text-dark-yellow font-semibold">Bem vindo!</h2>
+    <p class="italic text-md">
+      Meu nome é Diego, eu sou um desenvolvedor fullstack<span
+        class="text-dark-yellow blink">_</span
+      >
+    </p>
+  </div>
 
   <a
-    class="mt-4 text-dark-yellow hover:text-grey transition-colors text-sm font-mono before:content-['$'] before:mr-2"
+    class="text-lg mt-6 text-dark-yellow hover:text-dark-yellow/70 font-bold before:content-['$'] before:text-dark-yellow hover:before:text-dark-yellow/70 before:mr-2"
     href="/projects"
   >
     cd ~/code/projects/
